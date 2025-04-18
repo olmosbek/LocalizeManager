2b1bce150f8e792145fdf15253f3b17b5711cdbb
olmosbek <olmosbek.2444@gmail.com>
2025-04-18 16:37:01 +0500

    Update LocalizeManager.swift

--- Sources/LocalizeManager/LocalizeManager.swift
+++ Sources/LocalizeManager/LocalizeManager.swift
@@ -1,2 +1,17 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+import SwiftUI
+
+#if os(iOS)
+@available(iOS 16.0, *)
+public struct LocalizeView: View {
+    public init() {}
+    public var body: some View {
+        ZStack {
+            Color.red.ignoresSafeArea(.all)
+        }
+    }
+}
+#endif
